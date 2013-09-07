6f6a7b7c6cb44f13faba9d62882c732268f2a160
Ofer Ben-Noon <oferben7669@gmail.com>
2013-09-07 01:27:47 -0400

    Implementing spotify.getRecommendationsForUser

--- spotifyprovider.js
+++ spotifyprovider.js
@@ -72,10 +72,60 @@ SpotifyProvider.prototype.getPrivateRatingCollection = function (callback) {
     });
 };
 
+// In case of an error, calls the error handler
+function getTopSongsForUser(collection, user_id, top_number, sort_by, error_handler, in_callback) {
+    top_number = parseInt(top_number);
+    var return_results = function (error, songs_array) {
+        if (error) {
+            error_handler(error);
+            return;
+        }
 
+        for (var i = 0; i < songs_array.length; i++) {
+            songs_array[i].rank = i;
+        }
+
+        in_callback(songs_array);
+    };
+    if (user_id != null) {
+        collection.find({user_id: user_id}).sort(sort_by).limit(top_number).toArray(return_results);
+    } else {
+        collection.find().sort(sort_by).limit(top_number).toArray(return_results);
+    }
+}
+
+SpotifyProvider.prototype.returnTopRated = function (recommendation_result, collection, user_id, top_number, callback) {
+    // In case of an error, getTopSongsForUser calls the callback function with the error value.
+    getTopSongsForUser(collection, user_id, top_number, {meditation: -1}, callback, function (result) {
+        recommendation_result.meditation = result;
+        getTopSongsForUser(collection, user_id, top_number, {engagement: -1}, callback, function (result) {
+            recommendation_result.engagement = result;
+            getTopSongsForUser(collection, user_id, top_number, {happiness: 1}, callback, function (result) {
+                recommendation_result.happiness = result;
+                getTopSongsForUser(collection, user_id, top_number, {excitement: -1}, callback, function (result) {
+                    recommendation_result.excitement = result;
+                    callback(null, recommendation_result);
+                });
+            });
+        });
+    });
+};
 
 // Get the records in user private rating table
 SpotifyProvider.prototype.getRecommendationsForUser = function (user_id, top_number, callback) {
+    var recommendation_result = {
+        user_id: user_id,
+        top_number: top_number
+    };
+    var provider = this;
+    this.getPrivateRatingCollection(function (error, private_rating_collection) {
+        if (error) {
+            callback(error);
+            return;
+        } else {
+            provider.returnTopRated(recommendation_result, private_rating_collection, user_id, top_number, callback);
+        }
+    });
 
 };
 
